5091a48a193f6666e5eae768e8a70d4541deec3e
westlia <96121397+westlia@users.noreply.github.com>
2024-01-14 21:45:03 +0800

    修复github不再支持svn

--- diy-part1.sh
+++ diy-part1.sh
@@ -44,15 +44,9 @@ git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-svn export https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
-svn export https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
-svn export https://github.com/fw876/helloworld/trunk/v2raya package/v2raya
-svn export https://github.com/fw876/helloworld/trunk/redsocks2 package/redsocks2
-svn export https://github.com/fw876/helloworld/trunk/microsocks package/microsocks
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
-svn export https://github.com/kiddin9/openwrt-packages/trunk/cdnspeedtest package/cdnspeedtest
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-adguardhome package/luci-app-adguardhome
-svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+git clone --depth 1 https://github.com/fw876/helloworld package/helloworld && mv -n package/helloworld/luci-app-ssr-plus package && mv -n package/helloworld/lua-neturl package && mv -n package/helloworld/v2raya package && mv -n package/helloworld/redsocks2 package && mv -n package/helloworld/microsocks package ; rm -rf package/helloworld
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages package/openwrt-packages && mv -n package/openwrt-packages/luci-app-cloudflarespeedtest package && mv -n package/openwrt-packages/cdnspeedtest package && mv -n package/openwrt-packages/adguardhome package ; rm -rf package/openwrt-packages
+git clone --depth 1 https://github.com/kenzok8/small-package package/small-package  && mv -n package/small-package/luci-app-adguardhome package ; rm -rf package/small-package
 git clone --depth 1 https://github.com/sensec/luci-app-udp2raw package/luci-app-udp2raw
 sed -i 's/luci-app-unblockmusic luci-app-zerotier/luci-app-zerotier zram-swap ipv6helper openssh-sftp-server tailscale tinc/' target/linux/ipq807x/Makefile
 sed -i 's/ tinc/ tinc luci-theme-design luci-app-design-config luci-app-wireguard luci-app-dawn luci-app-easymesh luci-app-frpc luci-app-guest-wifi luci-app-haproxy-tcp luci-app-mosdns luci-app-mwan3 luci-app-mwan3helper luci-app-n2n luci-app-ntpc luci-app-pppoe-relay luci-app-pushbot luci-app-ramfree luci-app-socat luci-app-sqm luci-app-syncdial luci-app-tinyproxy luci-app-ttyd luci-app-udpxy luci-app-uhttpd luci-app-watchcat luci-app-webadmin luci-app-wifischedule/' target/linux/ipq807x/Makefile
